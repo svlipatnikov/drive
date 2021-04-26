73b8833ab9bdde83a544099f0062ba8db618b8fb
Lipatnikov <sv.lipatnikov@gmail.com>
2021-04-26 16:57:08 +0400

    feat add redux

---
+++ src/redux/reducers/mainReducer.js
@@ -0,0 +1,10 @@
+const mainReducedInit = {};
+
+const mainReducer = (state = mainReducedInit, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default mainReducer;
