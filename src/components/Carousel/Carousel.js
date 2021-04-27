2d73456fbfb812df655440d60e1fa633504b7bb2
SeregyLipatnikov <sv.lipatnikov@gmail.com>
2021-04-27 19:48:06 +0400

    feat carousel func

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,37 +1,64 @@
-import React from 'react';
-import carouselItems from './carouselItems';
+import React, { useState } from 'react';
+import items from './assets/items';
 import { ReactComponent as ArrowLeft } from 'assets/svg/arrowLeft.svg';
 import { ReactComponent as ArrowRight } from 'assets/svg/arrowRight.svg';
 import './carousel.scss';
 
 const Carousel = () => {
-  console.log(carouselItems);
+  const [current, setCurrent] = useState(0);
+
+  const btnGradient = {
+    background: `linear-gradient(90deg, ${items[current].btnColors[0]} 0%, ${items[current].btnColors[1]} 100%)`,
+  };
+
+  const handleNextClick = () => {
+    current < items.length - 1 ? setCurrent((current) => ++current) : setCurrent(0);
+  };
+
+  const handlePrevClick = () => {
+    current !== 0 ? setCurrent((current) => --current) : setCurrent(items.length - 1);
+  };
+
+  const handleDotClick = (index) => () => {
+    setCurrent(index);
+  };
 
   return (
     <section className="carousel">
-      <div>
-        <img src={carouselItems[0].src} alt="bacground" className="carousel__background-img" />
-      </div>
+      {items.map((item, index) => (
+        <img
+          key={items[index].src}
+          src={items[index].src}
+          alt="background"
+          className={`carousel__img ${index === current ? 'carousel__img--current' : ''}`}
+        />
+      ))}
 
-      <button className="carousel__arrow-btn carousel__arrow-btn--left">
+      <button className="carousel__arrow-btn carousel__arrow-btn--left" onClick={handlePrevClick}>
         <ArrowLeft />
       </button>
 
       <div className="carousel__content">
-        <h3 className="carousel__content__header">{carouselItems[0].header}</h3>
-        <p className="carousel__content__text">{carouselItems[0].text}</p>
-        <button className="carousel__content__button">Подробнее</button>
+        <h3 className="carousel__content__header">{items[current].header}</h3>
+        <p className="carousel__content__text">{items[current].text}</p>
+        <button className="carousel__content__button" style={btnGradient}>
+          Подробнее
+        </button>
       </div>
 
-      <button className="carousel__arrow-btn carousel__arrow-btn--right">
+      <button className="carousel__arrow-btn carousel__arrow-btn--right" onClick={handleNextClick}>
         <ArrowRight />
       </button>
 
       <div className="carousel__dots">
-        <div className="carousel__dots__item" />
-        <div className="carousel__dots__item" />
-        <div className="carousel__dots__item" />
-        <div className="carousel__dots__item" />
+        {items.map((item, index) => (
+          <button
+            className={`carousel__dots__item ${
+              index === current ? 'carousel__dots__item--current' : ''
+            }`}
+            onClick={handleDotClick(index)}
+          />
+        ))}
       </div>
     </section>
   );
---
+++ src/components/Carousel/assets/items.js
@@ -0,0 +1,37 @@
+import image1 from './images/image1.jpg';
+import image2 from './images/image2.jpg';
+import image3 from './images/image3.jpg';
+import image4 from './images/image4.jpg';
+
+const carouselItems = [
+  {
+    header: 'Бесплатная парковка',
+    text:
+      'Оставляйте машину на платных городских парковках и разрешенных местах, не нарушая ПДД, а также в аэропортах.',
+    src: image1,
+    btnColors: ['#13493F', '#0C7B1B'],
+  },
+
+  {
+    header: 'Страховка',
+    text: 'Полная страховка страховка автомобиля',
+    src: image2,
+    btnColors: ['#132949', '#0C7B67'],
+  },
+
+  {
+    header: 'Бензин',
+    text: 'Полный бак на любой заправке города за наш счёт',
+    src: image3,
+    btnColors: ['#493013', '#7B0C3B'],
+  },
+
+  {
+    header: 'Обслуживание',
+    text: 'Автомобиль проходит еженедельное ТО',
+    src: image4,
+    btnColors: ['#281349', '#720C7B'],
+  },
+];
+
+export default carouselItems;
