13f6e7b42235c19047ffa1f1d4ebd9be4035be4d
Lipatnikov <sv.lipatnikov@gmail.com>
2021-04-27 16:48:55 +0400

    carousel layout

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,8 +1,40 @@
 import React from 'react';
+import carouselItems from './carouselItems';
+import { ReactComponent as ArrowLeft } from 'assets/svg/arrowLeft.svg';
+import { ReactComponent as ArrowRight } from 'assets/svg/arrowRight.svg';
 import './carousel.scss';
 
 const Carousel = () => {
-  return <section className="carousel">Carousel</section>;
+  console.log(carouselItems);
+
+  return (
+    <section className="carousel">
+      <div>
+        <img src={carouselItems[0].src} alt="bacground" className="carousel__background-img" />
+      </div>
+
+      <button className="carousel__arrow-btn carousel__arrow-btn--left">
+        <ArrowLeft />
+      </button>
+
+      <div className="carousel__content">
+        <h3 className="carousel__content__header">{carouselItems[0].header}</h3>
+        <p className="carousel__content__text">{carouselItems[0].text}</p>
+        <button className="carousel__content__button">Подробнее</button>
+      </div>
+
+      <button className="carousel__arrow-btn carousel__arrow-btn--right">
+        <ArrowRight />
+      </button>
+
+      <div className="carousel__dots">
+        <div className="carousel__dots__item" />
+        <div className="carousel__dots__item" />
+        <div className="carousel__dots__item" />
+        <div className="carousel__dots__item" />
+      </div>
+    </section>
+  );
 };
 
 export default Carousel;
